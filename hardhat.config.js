763d6a1bb6fe49e8df21feaaa4a58bb75cd21b04
Yuriy Gulyayev <yuriygulyayev@gmail.com>
2025-02-08 18:30:26 -0500

    Upgraded Solidity version.

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("@nomicfoundation/hardhat-toolbox");
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: {
-		version: "0.8.27",
+		version: "0.8.28",
 
 		settings: {
 			evmVersion: "cancun",
